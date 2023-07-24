c6326aea01aa9f9651edb6af2956ff9daa1b308c
Gerald Kamau <11geraldmoney@gmail.com>
2023-07-24 13:35:27 +0300

    Setting up the routes

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,8 @@
+import { Routes, Route, Outlet, Navigate, useLocation } from "react-router-dom";
+
+const Index = () => {
+  const location = useLocation();
+  return <Routes></Routes>;
+};
+
+export { Index };
